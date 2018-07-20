a5ad3ef5127c1cdebda4084882789f90530325ed
Johannes Klein <johannes.t.klein@gmail.com>
2018-07-20 15:08:56 +0200

    Restructure navigator

--- inat-identify/App.js
+++ inat-identify/App.js
@@ -1,43 +1,9 @@
 import React from 'react';
-import { Button, View, Text } from "react-native";
-import { createStackNavigator } from "react-navigation";
-class HomeScreen extends React.Component {
 
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>Home Screen</Text>
-        <Button
-          title="Go to Details"
-          onPress={() => this.props.navigation.navigate("Details")}
-        />
-      </View>
-    );
-  }
-}
-
-class DetailsScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>Details Screen</Text>
-      </View>
-    );
-  }
-}
-
-const RootStack = createStackNavigator(
-  {
-    Home: HomeScreen,
-    Details: DetailsScreen
-  },
-  {
-    initialRouteName: "Home"
-  }
-);
+import IiAppNavigator from './src/IiAppNavigator';
 
 export default class App extends React.Component {
   render() {
-    return <RootStack />;
+    return <IiAppNavigator />;
   }
 }
---
+++ inat-identify/src/IiAppNavigator.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Button, View, Text } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+        <Button
+          title="Go to Details"
+          onPress={() => this.props.navigation.navigate('Details')}
+        />
+      </View>
+    );
+  }
+}
+
+class DetailsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+      </View>
+    );
+  }
+}
+
+const RootStack = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Details: DetailsScreen
+  },
+  {
+    initialRouteName: 'Home'
+  }
+);
+
+export default class IiAppNavigator extends React.Component {
+  render() {
+    return <RootStack />;
+  }
+}
