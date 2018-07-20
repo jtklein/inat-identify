355ab66e8073ee87a026c7337fd516bd9a1775bf
Johannes Klein <johannes.t.klein@gmail.com>
2018-07-20 15:38:51 +0200

    Add basic swiper

--- inat-identify/src/IiAppNavigator.js
+++ inat-identify/src/IiAppNavigator.js
@@ -1,13 +1,8 @@
 import React from 'react';
-import { Button, View, Text } from 'react-native';
 import { createStackNavigator } from 'react-navigation';
 
 import IiAuthScreen from './screens/IiAuthScreen';
 import IiIdentifyScreen from './screens/IiIdentifyScreen';
-
-class HomeScreen extends React.Component {
-  }
-}
 
 const RootStack = createStackNavigator(
   {
--- inat-identify/src/screens/IiIdentifyScreen.js
+++ inat-identify/src/screens/IiIdentifyScreen.js
@@ -1,12 +1,177 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { Component } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import Swiper from 'react-native-deck-swiper';
 
-export default class IiIdentifyScreen extends React.Component {
+export default class IiIdentifyScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cards: ['1', '2', '3'],
+      swipedAllCards: false,
+      swipeDirection: '',
+      isSwipingBack: false,
+      cardIndex: 0
+    };
+  }
+
+
+  renderCard = (card, index) => {
+    return (
+      <View style={styles.card}>
+        <Text style={styles.text}>
+          {card} - {index}
+        </Text>
+      </View>
+    );
+  };
+
+  onSwipedAllCards = () => {
+    this.setState({
+      swipedAllCards: true
+    });
+  };
+
+  swipeBack = () => {
+    if (!this.state.isSwipingBack) {
+      this.setIsSwipingBack(true, () => {
+        this.swiper.swipeBack(() => {
+          this.setIsSwipingBack(false);
+        });
+      });
+    }
+  };
+
+  setIsSwipingBack = (isSwipingBack, cb) => {
+    this.setState(
+      {
+        isSwipingBack: isSwipingBack
+      },
+      cb
+    );
+  };
+
+  swipeLeft = () => {
+    this.swiper.swipeLeft();
+  };
+
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Identify Screen</Text>
+      <View style={styles.container}>
+        <Swiper
+          ref={swiper => {
+            this.swiper = swiper;
+          }}
+          onSwiped={this.onSwiped}
+          onTapCard={this.swipeLeft}
+          cards={this.state.cards}
+          cardIndex={this.state.cardIndex}
+          cardVerticalMargin={80}
+          renderCard={this.renderCard}
+          onSwipedAll={this.onSwipedAllCards}
+          stackSize={3}
+          stackSeparation={15}
+          overlayLabels={{
+            bottom: {
+              title: 'BLEAH',
+              style: {
+                label: {
+                  backgroundColor: 'black',
+                  borderColor: 'black',
+                  color: 'white',
+                  borderWidth: 1
+                },
+                wrapper: {
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  justifyContent: 'center'
+                }
+              }
+            },
+            left: {
+              title: 'NOPE',
+              style: {
+                label: {
+                  backgroundColor: 'black',
+                  borderColor: 'black',
+                  color: 'white',
+                  borderWidth: 1
+                },
+                wrapper: {
+                  flexDirection: 'column',
+                  alignItems: 'flex-end',
+                  justifyContent: 'flex-start',
+                  marginTop: 30,
+                  marginLeft: -30
+                }
+              }
+            },
+            right: {
+              title: 'LIKE',
+              style: {
+                label: {
+                  backgroundColor: 'black',
+                  borderColor: 'black',
+                  color: 'white',
+                  borderWidth: 1
+                },
+                wrapper: {
+                  flexDirection: 'column',
+                  alignItems: 'flex-start',
+                  justifyContent: 'flex-start',
+                  marginTop: 30,
+                  marginLeft: 30
+                }
+              }
+            },
+            top: {
+              title: 'SUPER LIKE',
+              style: {
+                label: {
+                  backgroundColor: 'black',
+                  borderColor: 'black',
+                  color: 'white',
+                  borderWidth: 1
+                },
+                wrapper: {
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  justifyContent: 'center'
+                }
+              }
+            }
+          }}
+          animateOverlayLabelsOpacity
+          animateCardOpacity
+        >
+          <Button onPress={this.swipeLeft} title="Swipe Left" />
+        </Swiper>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FCFF'
+  },
+  card: {
+    flex: 1,
+    borderRadius: 4,
+    borderWidth: 2,
+    borderColor: '#E8E8E8',
+    justifyContent: 'center',
+    backgroundColor: 'white'
+  },
+  text: {
+    textAlign: 'center',
+    fontSize: 50,
+    backgroundColor: 'transparent'
+  },
+  done: {
+    textAlign: 'center',
+    fontSize: 30,
+    color: 'white',
+    backgroundColor: 'transparent'
+  }
+});
