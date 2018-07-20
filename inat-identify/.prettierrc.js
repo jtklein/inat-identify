f2c9534cb4d6f9d0cfbd2afd7bb390d5bfb872d4
Johannes Klein <johannes.t.klein@gmail.com>
2018-07-20 15:03:02 +0200

    Add some configs for the project

---
+++ inat-identify/.prettierrc.js
@@ -0,0 +1,10 @@
+// .prettierrc.js
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+};
