21752d065d98667b3a00e4db25827e22f6cff9f3
Johannes Klein <johannes.t.klein@gmail.com>
2018-07-20 14:42:42 +0200

    Add react-navigation

--- inat-identify/App.js
+++ inat-identify/App.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from "react-navigation";
+class HomeScreen extends React.Component {
 
-export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+export default createStackNavigator({
+  Home: {
+    screen: HomeScreen
   },
 });
