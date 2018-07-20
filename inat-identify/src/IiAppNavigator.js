7ecbefb0f408958e7e2d648885fee6d3af69d2db
Johannes Klein <johannes.t.klein@gmail.com>
2018-07-20 15:16:06 +0200

    Destructure screen two

--- inat-identify/src/IiAppNavigator.js
+++ inat-identify/src/IiAppNavigator.js
@@ -1,28 +1,21 @@
 import React from 'react';
 import { Button, View, Text } from 'react-native';
 import { createStackNavigator } from 'react-navigation';
+
 import IiAuthScreen from './screens/IiAuthScreen';
+import IiIdentifyScreen from './screens/IiIdentifyScreen';
+
 class HomeScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-        <Button
-          title="Go to Details"
-          onPress={() => this.props.navigation.navigate('Auth')}
-        />
-      </View>
-    );
   }
 }
 
 const RootStack = createStackNavigator(
   {
     Auth: IiAuthScreen,
-    Home: HomeScreen
+    Identify: IiIdentifyScreen
   },
   {
-    initialRouteName: 'Home'
+    initialRouteName: 'Auth'
   }
 );
 
--- inat-identify/src/screens/IiAuthScreen.js
+++ inat-identify/src/screens/IiAuthScreen.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 
 export default class IiAuthScreen extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Auth Screen</Text>
+        <Button
+          title="Go to Identify"
+          onPress={() => this.props.navigation.navigate('Identify')}
+        />
       </View>
     );
   }
