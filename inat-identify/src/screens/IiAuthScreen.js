8e4308191a2de1e9bf9e95583473bfe7317f3411
Johannes Klein <johannes.t.klein@gmail.com>
2018-10-08 13:06:39 +0200

    Add iNaturalist app secrets

--- inat-identify/src/screens/IiAuthScreen.js
+++ inat-identify/src/screens/IiAuthScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text, Button } from 'react-native';
+import oauth from '../../secrets/oauth';
 
 export default class IiAuthScreen extends React.Component {
   render() {
