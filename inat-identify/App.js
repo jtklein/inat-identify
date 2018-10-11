41523f61b276eb9f156c38b86f98bfd8417f79f4
Johannes Klein <johannes.t.klein@gmail.com>
2018-10-11 12:46:59 +0200

    Add react native paper

--- inat-identify/App.js
+++ inat-identify/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 
 import IiAppNavigator from './src/IiAppNavigator';
 
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+  }
+};
+
 export default class App extends React.Component {
   render() {
-    return <IiAppNavigator />;
+    return (
+      <PaperProvider theme={theme}>
+        <IiAppNavigator />
+      </PaperProvider>
+    );
   }
 }
