67ed6a3e3a80c2038d04324c88c84437a65ebd46
Johannes Klein <johannes.t.klein@gmail.com>
2018-10-12 18:21:23 +0200

    Refactor to switch as the primary navigator

--- inat-identify/src/IiAppNavigator.js
+++ inat-identify/src/IiAppNavigator.js
@@ -1,24 +1,78 @@
 import React from 'react';
-import { createStackNavigator } from 'react-navigation';
+import { createStackNavigator, createSwitchNavigator } from 'react-navigation';
 
 import IiAuthScreen from './screens/IiAuthScreen';
 import IiIdentifyScreen from './screens/IiIdentifyScreen';
 import ItSettingsScreen from './screens/ItSettingsScreen';
 
-const RootStack = createStackNavigator(
+import { colors } from './styles';
+
+const { primaryColor } = colors;
+
+const headerStyle = {
+  backgroundColor: primaryColor
+};
+const headerTintColor = '#FFFFFF';
+const headerTitleStyle = {
+  fontWeight: 'bold'
+};
+
+const AuthStack = createStackNavigator(
   {
-    Auth: IiAuthScreen,
+    Auth: IiAuthScreen
+  },
+  {
+    initialRouteName: 'Auth',
+    navigationOptions: {
+      header: null,
+      gesturesEnabled: false
+    }
+  }
+);
+
+const AppStack = createStackNavigator(
+  {
     Settings: ItSettingsScreen,
     Identify: IiIdentifyScreen
   },
   {
-    initialRouteName: 'Auth'
+    initialRouteName: 'Settings',
+    navigationOptions: {
+      headerStyle,
+      headerTintColor,
+      headerTitleStyle,
+    }
   }
 );
 
+/**
+ * The {@link SwitchNavigator} entry point of the entire app. Consists of launcher stack with
+ * user authentication logic, and the main navigation object for the app.
+ * @type {SwitchNavigator}
+ */
+const RootNavigator = createSwitchNavigator(
+  // RouteConfigs
+  {
+    Auth: AuthStack,
+    App: AppStack
+  },
+  // createSwitchNavigatorConfig
+  {
+
+    initialRoutName: 'Auth',
+    resetOnBlur: true,
+    backBehavior: 'none',
+    navigationOptions: {
+      header: null,
+      gesturesEnabled: false,
+    },
+  },
+);
+
+
 export default class IiAppNavigator extends React.Component {
   render() {
-    return <RootStack />;
+    return <RootNavigator />;
   }
 }
 
