05503e4ced8be5da296ed741719904c3f03fdff3
Johannes Klein <johannes.t.klein@gmail.com>
2018-10-16 00:31:39 +0200

    Add redux store to app

--- inat-identify/src/ItApp.js
+++ inat-identify/src/ItApp.js
@@ -1,21 +1,27 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { Provider as PaperProvider } from 'react-native-paper';
 
 import ItAppContainer from './ItAppContainer';
 
+import configureStore from './store';
 import { theme } from './styles';
 
 if (!__DEV__) {
   console.log = () => {};
 }
 
+const store = configureStore();
+
 class ItApp extends React.Component {
   render() {
     return (
 
+      <Provider store={store}>
         <PaperProvider theme={theme}>
           <ItAppContainer />
         </PaperProvider>
+      </Provider>
     );
   }
 }
