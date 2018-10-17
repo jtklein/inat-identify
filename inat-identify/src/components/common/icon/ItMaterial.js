d6b8c1c1d5e284b13ad4c6f5f4300e239d2dfe89
Johannes Klein <johannes.t.klein@gmail.com>
2018-10-17 12:27:57 +0200

    Add component for an icon

---
+++ inat-identify/src/components/common/icon/ItMaterial.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { MaterialIcons } from '@expo/vector-icons';
+
+import { colors } from '../../../styles';
+
+/**
+ * The primary styled component to use a MaterialIcon from Expo.
+ */
+const ItMaterial = props => {
+  const { primaryColor } = colors;
+  const { size, color } = props;
+  return (
+    <MaterialIcons {...props} size={size || 24} color={color || primaryColor} />
+  );
+};
+
+export { ItMaterial };
