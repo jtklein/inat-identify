860e574db248125f5479dfdc785168c51d5e7d9e
Johannes Klein <johannes.t.klein@gmail.com>
2018-11-09 18:28:21 +0100

    Upgrade to expo sdk 31

--- inat-identify/src/ItApp.js
+++ inat-identify/src/ItApp.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Expo from 'expo';
+import * as Expo from 'expo';
 import { Provider } from 'react-redux';
 import { Provider as PaperProvider } from 'react-native-paper';
 
