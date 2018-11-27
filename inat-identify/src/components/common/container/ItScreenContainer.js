6772eb33fa681f1200c0ec036f7206adb7704a79
Johannes Klein <johannes.t.klein@gmail.com>
2018-11-27 20:25:04 +0100

    Remove styling

--- inat-identify/src/components/common/container/ItScreenContainer.js
+++ inat-identify/src/components/common/container/ItScreenContainer.js
@@ -22,7 +22,6 @@ const ItScreenContainer = (props) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
     padding: 16,
     backgroundColor: '#FFFFFF',
   },
