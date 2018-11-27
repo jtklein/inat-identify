b4070e8e190a86d2cba626b63b3e70174fb4bd02
Johannes Klein <johannes.t.klein@gmail.com>
2018-11-27 20:30:29 +0100

    Refactor swiper to be set up in this project directly, with usage of react native reanimated

---
+++ inat-identify/src/components/features/swiper/ItSwiper.js
@@ -0,0 +1,277 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, Dimensions } from 'react-native';
+import { PanGestureHandler, State } from 'react-native-gesture-handler';
+import Animated from 'react-native-reanimated';
+
+import ItSwiperCard from './ItSwiperCard';
+
+function runSpring(clock, value, dest) {
+  const state = {
+    finished: new Value(0),
+    velocity: new Value(0),
+    position: new Value(0),
+    time: new Value(0),
+  };
+  const config = {
+    tension: 60,
+    speed: 14,
+    damping: 20,
+    mass: 1,
+    stiffness: 100,
+    overshootClamping: false,
+    restSpeedThreshold: 1,
+    restDisplacementThreshold: 0.5,
+    toValue: new Value(0),
+  };
+  return [
+    cond(clockRunning(clock), 0, [
+      set(state.finished, 0),
+      set(state.velocity, 0),
+      set(state.position, value),
+      set(config.toValue, dest),
+      startClock(clock),
+    ]),
+    spring(clock, state, config),
+    cond(state.finished, stopClock(clock)),
+    state.position,
+  ];
+}
+
+const { width, height } = Dimensions.get('window');
+const toRadians = angle => angle * (Math.PI / 180);
+const rotatedWidth = width * Math.sin(toRadians(90 - 15)) + height * Math.sin(toRadians(15));
+const {
+  add,
+  multiply,
+  neq,
+  spring,
+  cond,
+  eq,
+  event,
+  lessThan,
+  greaterThan,
+  and,
+  call,
+  set,
+  clockRunning,
+  startClock,
+  stopClock,
+  Clock,
+  Value,
+  concat,
+  interpolate,
+  Extrapolate,
+} = Animated;
+
+class ItSwiper extends Component {
+  constructor(props) {
+    super(props);
+    const { observations } = props;
+    this.state = { observations };
+    this.translationX = new Value(0);
+    this.translationY = new Value(0);
+    this.velocityX = new Value(0);
+    this.velocityY = new Value(0);
+    this.offsetY = new Value(0);
+    this.offsetX = new Value(0);
+    this.gestureState = new Value(State.UNDETERMINED);
+    this.onGestureEvent = event(
+      [
+        {
+          nativeEvent: {
+            translationX: this.translationX,
+            translationY: this.translationY,
+            velocityX: this.velocityX,
+            velocityY: this.velocityY,
+            state: this.gestureState,
+          },
+        },
+      ],
+      { useNativeDriver: true },
+    );
+    this.init();
+  }
+
+  init = () => {
+    const clockX = new Clock();
+    const clockY = new Clock();
+    const {
+      translationX,
+      translationY,
+      velocityX,
+      velocityY,
+      gestureState,
+      offsetY,
+      offsetX,
+    } = this;
+    gestureState.setValue(State.UNDETERMINED);
+    translationX.setValue(0);
+    translationY.setValue(0);
+    velocityX.setValue(0);
+    velocityY.setValue(0);
+    offsetY.setValue(0);
+    offsetX.setValue(0);
+
+    const finalTranslateX = add(translationX, multiply(0.2, velocityX));
+    const translationThresholdX = width / 4;
+    const snapPointX = cond(
+      lessThan(finalTranslateX, -translationThresholdX),
+      -rotatedWidth,
+      cond(greaterThan(finalTranslateX, translationThresholdX), rotatedWidth, 0),
+    );
+
+    const finalTranslateY = add(translationY, multiply(0.5, velocityY));
+    const translationThresholdY = height / 4;
+    const snapPointY = cond(
+      lessThan(finalTranslateY, -translationThresholdY),
+      -rotatedWidth,
+      cond(greaterThan(finalTranslateY, translationThresholdY), rotatedWidth, 0),
+    );
+
+    // TODO: handle case where the user drags the card again before the spring animation finished
+    this.translateY = cond(
+      eq(gestureState, State.END),
+      [
+        set(translationY, runSpring(clockY, translationY, snapPointY)),
+        set(offsetY, translationY),
+        cond(and(eq(clockRunning(clockY), 0), neq(translationY, 0)), [
+          call([translationY], this.onVerticalSwiped),
+        ]),
+        translationY,
+      ],
+      cond(eq(gestureState, State.BEGAN), [stopClock(clockY), translationY], translationY),
+    );
+
+    this.translateX = cond(
+      eq(gestureState, State.END),
+      [
+        set(translationX, runSpring(clockX, translationX, snapPointX)),
+        set(offsetX, translationX),
+        cond(and(eq(clockRunning(clockX), 0), neq(translationX, 0)), [
+          call([translationX], this.onHorizontalSwiped),
+        ]),
+        translationX,
+      ],
+      cond(eq(gestureState, State.BEGAN), [stopClock(clockX), translationX], translationX),
+
+    );
+  };
+
+  onHorizontalSwiped = ([translationX]) => {
+    const { observations: [topObservation, ...observations] } = this.state;
+    const {
+      onSwipedLeft,
+      onSwipedRight,
+      onSwipedAllCards
+    } = this.props;
+    const goesDown = translationX > 0;
+    // Card was swiped right
+    if (goesDown) {
+      onSwipedRight(topObservation);
+    } else {
+      onSwipedLeft(topObservation);
+    }
+    this.setState({ observations }, this.init);
+  }
+
+  onVerticalSwiped = ([translationY]) => {
+    const { observations: [topObservation, ...observations] } = this.state;
+    const {
+      onSwipedTop,
+      onSwipedBottom,
+      onSwipedAllCards
+    } = this.props;
+    const goesBottom = translationY > 0;
+    // Card was swiped right
+    if (goesBottom) {
+      onSwipedBottom(topObservation);
+    } else {
+      onSwipedTop(topObservation);
+    }
+    this.setState({ observations }, this.init);
+  }
+
+  render() {
+    const { onGestureEvent, translateX, translateY } = this;
+    const { observations: [topObservation, ...observations] } = this.state;
+    const { swiper } = this.props;
+    const { cards } = styles;
+
+    const leftOpacity = interpolate(translateX, {
+      inputRange: [-width / 2, 0],
+      outputRange: [1, 0],
+    });
+    const rightOpacity = interpolate(translateX, {
+      inputRange: [0, width / 2],
+      outputRange: [0, 1],
+    });
+    const topOpacity = interpolate(translateY, {
+      inputRange: [-height / 2, 0],
+      outputRange: [1, 0],
+    });
+    const bottomOpacity = interpolate(translateY, {
+      inputRange: [0, height / 2],
+      outputRange: [0, 1],
+    });
+
+    const rotateZ = concat(
+      interpolate(translateX, {
+        inputRange: [-width / 2, width / 2],
+        outputRange: [15, -15],
+        extrapolate: Extrapolate.CLAMP,
+      }),
+      'deg',
+    );
+
+    const style = {
+      ...StyleSheet.absoluteFillObject,
+      zIndex: 900,
+      transform: [
+        { translateX },
+        { translateY },
+        { rotateZ },
+      ],
+    };
+
+    return (
+      <View style={cards}>
+        {
+          observations.reverse().map(observation => (
+            <ItSwiperCard
+              key={observation.id}
+              swiper={swiper}
+              {...{ observation }}
+            />
+          ))
+        }
+        <PanGestureHandler
+          onHandlerStateChange={onGestureEvent}
+          {...{ onGestureEvent }}
+        >
+          <Animated.View {...{ style }}>
+            <ItSwiperCard
+              swiper={swiper}
+              observation={topObservation}
+              {...{
+                rightOpacity,
+                leftOpacity,
+                topOpacity,
+                bottomOpacity,
+              }}
+            />
+          </Animated.View>
+        </PanGestureHandler>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  cards: {
+    flex: 1,
+    margin: 8,
+    zIndex: 100,
+  },
+});
+
+export default ItSwiper;
--- inat-identify/src/screens/IiIdentifyScreen.js
+++ inat-identify/src/screens/IiIdentifyScreen.js
@@ -3,20 +3,20 @@ import { Alert } from 'react-native';
 import { connect } from 'react-redux';
 import inatjs from 'inaturalistjs';
 
-import ItSwiper from '../components/features/ItSwiper';
+import ItSwiper from '../components/features/swiper/ItSwiper';
 import {
   ItScreenContainer,
   ItSpinner,
 } from '../components/common';
 
 class IiIdentifyScreen extends Component {
-  static navigationOptions = ({ navigation }) => ({ title: navigation.getParam('title') });
+  static navigationOptions = ({ navigation }) => ({
+    title: navigation.getParam('title')
+  });
 
   INITIAL_STATE = {
     user: {},
     apiToken: this.props.navigation.state.params.apiToken,
-    swipeDirection: '',
-    cardIndex: 0,
     observations: [],
     page: 0,
   };
@@ -47,36 +47,30 @@ class IiIdentifyScreen extends Component {
     return true;
   }
 
-  onSwipedLeft(index) {
-    const { observations } = this.state;
+  onSwipedLeft(observation) {
     const { swiper } = this.props;
     const { swipeLeft } = swiper;
-    this.setState({ cardIndex: index + 1 });
     // Use set id for this identification
-    this.identifyInAnimationFrame(observations[index], swipeLeft);
+    this.identifyInAnimationFrame(observation, swipeLeft);
   }
 
-  onSwipedRight(index) {
-    const { observations } = this.state;
+  onSwipedRight(observation) {
     const { swiper } = this.props;
     const { swipeRight } = swiper;
-    this.setState({ cardIndex: index + 1 });
     // Use set id for this identification
-    this.identifyInAnimationFrame(observations[index], swipeRight);
+    this.identifyInAnimationFrame(observation, swipeRight);
   }
 
-  onSwipedTop(index) {
-    const { observations } = this.state;
+  onSwipedTop(observation) {
     const { swiper } = this.props;
     const { swipeTop } = swiper;
-    this.setState({ cardIndex: index + 1 });
     // Use set id for this identification
-    this.identifyInAnimationFrame(observations[index], swipeTop);
+    this.identifyInAnimationFrame(observation, swipeTop);
   }
 
-  onSwipedBottom(index) {
-    this.setState({ cardIndex: index + 1 });
+  onSwipedBottom(observation) {
     // Nothing to do here, as this is the skip option
+    console.log('Skipped this observation', observation);
   }
 
   onSwipedAllCards = () => {
@@ -90,18 +84,18 @@ class IiIdentifyScreen extends Component {
     const options = { api_token: apiToken };
     return await inatjs.users
       .me(options)
-      .then((r) => {
+      .then(r => {
         this.setState({ user: r.results[0] });
         return r;
       })
       // TODO: UI response
-      .catch((e) => {
+      .catch(e => {
         console.log('Error in fetching current user', e);
         console.log(e.response);
         // Show alert for failure of getting user object from iNat
         Alert.alert(
           'Sorry',
-          'Unfortunately, something went wrong. You can not proceed.',
+          'Unfortunately, something went wrong. You can not proceed.'
         );
       });
   };
@@ -120,25 +114,27 @@ class IiIdentifyScreen extends Component {
       // Observations have been reviewed by the user with ID equal to the value of the viewer_id parameter
       viewer_id: user.id,
       reviewed: 'false',
-      photos: 'true',
+      photos: 'true'
     };
 
     inatjs.observations
       .search(params)
-      .then((rsp) => {
-        const filteredResults = rsp.results.filter(d => !d.species_guess).filter(d => d.photos.length <= maxPhotos);
+      .then(rsp => {
+        const filteredResults = rsp.results
+          .filter(d => !d.species_guess)
+          .filter(d => d.photos.length <= maxPhotos);
         this.setState({
           observations: filteredResults,
           page: rsp.page,
-          cardIndex: 0,
+          cardIndex: 0
         });
       })
-      .catch((e) => {
+      .catch(e => {
         console.log('Error in fetching list of observations', e);
         console.log(e.response);
         Alert.alert(
           'Sorry',
-          'Unfortunately, something went wrong. You can not proceed.',
+          'Unfortunately, something went wrong. You can not proceed.'
         );
       });
   }
@@ -150,7 +146,7 @@ class IiIdentifyScreen extends Component {
   }
 
   identify(observation, swipeOption) {
-    console.log('observation', observation);
+    console.log('Identified as, this', swipeOption.label, observation);
     const { apiToken } = this.state;
     /*
     {
@@ -171,30 +167,30 @@ class IiIdentifyScreen extends Component {
     const options = { api_token: apiToken };
     inatjs.identifications
       .create(identification, options)
-      .then((c) => {
+      .then(c => {
         console.log('identification', c);
         if (!swipeOption.subscribe) {
           inatjs.observations
             .subscribe(observation, options)
             .then(rsp => console.log('Unsubscriped to: ', rsp))
-            .catch((e) => {
+            .catch(e => {
               console.log('Error in unsubscribing to observation', e);
               console.log(e.response);
             });
         }
       })
-      .catch((e) => {
+      .catch(e => {
         console.log('Error in creating identification', e);
         console.log(e.response);
         Alert.alert(
           'Sorry',
-          'Unfortunately, something went wrong. Your identification was not processed.',
+          'Unfortunately, something went wrong. Your identification was not processed.'
         );
       });
   }
 
   render() {
-    const { observations, cardIndex } = this.state;
+    const { observations } = this.state;
     const { swiper } = this.props;
     if (!observations || !observations.length > 0) {
       return <ItSpinner />;
@@ -203,12 +199,11 @@ class IiIdentifyScreen extends Component {
       <ItScreenContainer>
         <ItSwiper
           observations={observations}
-          cardIndex={cardIndex}
           swiper={swiper}
-          onSwipedLeft={index => this.onSwipedLeft(index)}
-          onSwipedRight={index => this.onSwipedRight(index)}
-          onSwipedTop={index => this.onSwipedTop(index)}
-          onSwipedBottom={index => this.onSwipedBottom(index)}
+          onSwipedLeft={observation => this.onSwipedLeft(observation)}
+          onSwipedRight={observation => this.onSwipedRight(observation)}
+          onSwipedTop={observation => this.onSwipedTop(observation)}
+          onSwipedBottom={observation => this.onSwipedBottom(observation)}
           onSwipedAll={this.onSwipedAllCards}
         />
       </ItScreenContainer>
