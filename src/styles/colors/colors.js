491dcbe21477c8479ecfccec3e7b679ddd8afcd0
Johannes Klein <johannes.t.klein@gmail.com>
2020-11-12 13:30:10 +0100

    Add transparent primary color

--- src/styles/colors/colors.js
+++ src/styles/colors/colors.js
@@ -1,9 +1,11 @@
 import { DefaultTheme } from 'react-native-paper';
 
 const primaryColor = '#85A833';
+const transparentPrimaryColor = 'rgba(133, 168, 51, 0.3)';
 
 const colors = {
   primaryColor,
+  transparentPrimaryColor,
 };
 
 const theme = {
