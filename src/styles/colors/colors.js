88f569d600be66f533c86465c65b71a925db25fd
Johannes Klein <johannes.t.klein@gmail.com>
2020-11-16 15:28:52 +0100

    Add colors

--- src/styles/colors/colors.js
+++ src/styles/colors/colors.js
@@ -2,10 +2,14 @@ import { DefaultTheme } from 'react-native-paper';
 
 const primaryColor = '#85A833';
 const transparentPrimaryColor = 'rgba(133, 168, 51, 0.3)';
+const textColor = '#555555';
+const disabledColor = '#DDDDDD';
 
 const colors = {
   primaryColor,
   transparentPrimaryColor,
+  textColor,
+  disabledColor,
 };
 
 const theme = {
@@ -14,6 +18,8 @@ const theme = {
     ...DefaultTheme.colors,
     primary: primaryColor,
     accent: primaryColor,
+    text: textColor,
+    disabled: disabledColor,
   },
 };
 
