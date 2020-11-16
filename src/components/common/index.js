2727e4e31ddcbfe6107801e19793d16312bc8352
Johannes Klein <johannes.t.klein@gmail.com>
2020-11-16 15:26:36 +0100

    Export text component

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -2,3 +2,4 @@ export * from './container/ItScreenContainer';
 export * from './icon/ItMaterial';
 export * from './icon/ItHeaderButtons';
 export * from './spinner/ItSpinner';
+export * from './text/ItText';
