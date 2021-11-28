764c70c6057a55535de7d0e1cb30352db198b582
Johannes Klein <johannes.t.klein@gmail.com>
2021-11-28 23:26:37 +0100

    Remove chalk usage

--- scripts/CheckSecretsExist.js
+++ scripts/CheckSecretsExist.js
@@ -1,22 +1,17 @@
 const path = require('path');
-const chalk = require('chalk');
 const fs = require('fs');
 
 function CheckSecretsExist() {
   const secrets = path.join(__dirname, '..', 'secrets', 'oauth.json');
   if (!fs.existsSync(secrets)) {
     throw new Error(
-      chalk.whiteBright.bgRed.bold(
-        'The secrets file is not given. OAuth wit iNaturalist requires a JSON file with an object with the following keys INATURALIST_APP_ID and INATURALIST_APP_SECRET',
-      ),
+      'The secrets file is not given. OAuth wit iNaturalist requires a JSON file with an object with the following keys INATURALIST_APP_ID and INATURALIST_APP_SECRET'
     );
   }
   const apiToken = path.join(__dirname, '..', 'secrets', 'api_token.json');
   if (!fs.existsSync(apiToken)) {
     throw new Error(
-      chalk.whiteBright.bgRed.bold(
-        'The api_token file is not given. It is only used in DEV mode by now. Remains required though.',
-      ),
+      'The api_token file is not given. It is only used in DEV mode by now. Remains required though.'
     );
   }
 }
